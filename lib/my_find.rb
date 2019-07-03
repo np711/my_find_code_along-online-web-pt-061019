9602aea70c7272640116874644bcd3a82f257c6e
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-03 14:43:21 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
-def my_find(collection)
-   {|1| 1 % 3 == 0 and 1 % 5 == 0 }
-    
-  yield(1)
+ef my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
   end
-
 end
